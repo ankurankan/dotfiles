970d80ac11912f6e7143e9c38623517e14ed4dd0
Ankur Ankan <ankurankan@gmail.com>
2017-12-05 18:42:57 +0100

    adds script to start/play/pause spotify

---
+++ .scripts/spotify.sh
@@ -0,0 +1,1 @@
+(dbus-send --print-reply=literal --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause) || ((spotify &); sleep 3; dbus-send --print-reply=literal --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause)
